22f39fb5b23d2f5499d048107a33a7e833389c62
Kazinja@24 <azamabubaka511@gmail.com>
2026-02-05 00:00:46 +0300

    fix settings package and providing makemigrations solutions

--- users/models.py
+++ users/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
 # Create your models here.
+import uuid
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+
+class User(AbstractBaseUser, PermissionsMixin):
+    class Role(models.TextChoices):
+        TENANT = "TENANT"
+        LANDLORD = "LANDLORD"
+        ADMIN = "ADMIN"
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=255)
+    role = models.CharField(max_length=20, choices=Role.choices)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+
+from django.contrib.auth.models import BaseUserManager
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, role=None):
+        if not email:
+            raise ValueError("Email required")
+
+        user = self.model(email=self.normalize_email(email), role=role)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password, role="ADMIN")
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+        return user
+
+objects = UserManager()
